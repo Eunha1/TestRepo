8c82a2e6c50002dd1428e09d784c69eb8ce9a655
Eunha1 <doquangphuc18102001@gmail.com>
2023-12-30 18:56:59 +0700

    test merge branch_b

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-    CountingSort countSort = new CountingSort(10);
+    RadixSort countSort = new RadixSort(10);
         int[] sortedArray = countSort.getArray();
         System.out.println("unSorted array:");     
         printArray(sortedArray);
